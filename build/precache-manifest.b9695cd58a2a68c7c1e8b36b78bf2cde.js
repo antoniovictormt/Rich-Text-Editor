c1671216a5348e8112f47695ba4cf091940c8fbf
antoniovictormt <antoniovictormt@hotmail.com>
2020-04-14 16:24:15 -0300

    front-end

---
+++ build/precache-manifest.b9695cd58a2a68c7c1e8b36b78bf2cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a41747f5094cb97dd6a642c66f3a298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec55f3c1c3a3779b0e4c",
+    "url": "/static/css/main.33c71f8e.chunk.css"
+  },
+  {
+    "revision": "40b4ca0c7a4f657fe6a1",
+    "url": "/static/js/2.5e150a6d.chunk.js"
+  },
+  {
+    "revision": "a2c48be6fa8e582acc988bf29e254253",
+    "url": "/static/js/2.5e150a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec55f3c1c3a3779b0e4c",
+    "url": "/static/js/main.1dface04.chunk.js"
+  },
+  {
+    "revision": "ebdc029a03f028598687",
+    "url": "/static/js/runtime-main.9c13f983.js"
+  }
+]);
